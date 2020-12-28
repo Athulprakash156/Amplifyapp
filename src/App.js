4473e4b21874cd7f371201d9369d647cd7516203
Athulprakash156 <amstarathul@gmail.com>
2020-12-28 23:14:47 +0530

    crud operations success

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation, updateNote as updateNoteMutation } from './graphql/mutations';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const initialFormState = { name: '', description: '' }
 
 function App() {
   const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
+  const [isUpdate, setUpdate] = useState(false);
 
   useEffect(() => {
     fetchNotes();
@@ -29,15 +32,38 @@ function App() {
   }
 
   async function createNote() {
-    if (!formData.name || !formData.description) return;
+      if(isUpdate){
+        if (!formData.name || !formData.description) return;
+        console.log("formData",formData)
+        await API.graphql({ query: updateNoteMutation, variables: { input: formData } });
+        // await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    fetchNotes();
+    setUpdate(false);
+    setFormData(initialFormState);
+      }
+      else{
+        if (!formData.name || !formData.description) return;
     await API.graphql({ query: createNoteMutation, variables: { input: formData } });
     if (formData.image) {
       const image = await Storage.get(formData.image);
       formData.image = image;
     }
-  
-    setNotes([ ...notes, formData ]);
+    // setNotes([ ...notes, formData ]);
+    fetchNotes();
     setFormData(initialFormState);
+      }
+    
+  }
+
+  async function updateNote({id,name,description}){
+    let request={id, name, description};
+    setFormData(request);
+    setUpdate(true);
   }
 
   async function deleteNote({ id }) {
@@ -55,37 +81,47 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
-        value={formData.description}
-      />
-      <input
-       type="file"
-       onChange={onChange}
-      />
-      <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
-        {
-          notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteNote(note)}>Delete note</button>
-              {
-            note.image && <img src={note.image}  style={{width: 400}} />
-           }
-            </div>
-          ))
-        }
-      </div>
-      <AmplifySignOut />
+        <style>{'body { background-color: #85929E ; }'}</style>
+        <h1>My Notes App</h1>
+        <input
+         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+         placeholder="Note name"
+         value={formData.name}
+        />&nbsp;&nbsp;
+        <input
+         onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+         placeholder="Note description"
+         value={formData.description}
+       />&nbsp;&nbsp;
+       <input
+        type="file"
+        onChange={onChange}
+       />
+       <button onClick={createNote}>{isUpdate?"Update Node":"Create Note"}</button>
+         <div style={{marginBottom: 30,marginTop:30}}>
+           <table>
+           <thead>
+            <tr>
+             <th>Name</th>
+             <th>Description</th>
+             <th>Pictures</th>
+             <th>Actions</th>
+           </tr>
+          </thead>
+         <tbody> 
+           {notes.map((note,index) => (
+               <tr key={index} className="table-row">
+               <td>{ note.name }</td>
+               <td>{ note.description}</td>
+               <td>{ note.image && <img src={note.image}/>}</td>
+               <td><button onClick={() => deleteNote(note)}>Delete note</button>
+               <button className="end-button" onClick={() =>updateNote(note)}>Update note</button></td>
+              </tr>
+           ))}
+        </tbody>
+        </table>
+         </div>   
+       <AmplifySignOut />
     </div>
   );
 }
---
+++ src/updateUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { useTable } from 'react-table';
+class UpdateUser extends Component {
+    render(){
+        return(
+            <div>
+                <h1>EDIT USER</h1>
+                
+            </div>
+        )
+    }
+}
+export default UpdateUser;
